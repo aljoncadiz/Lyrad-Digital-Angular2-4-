0863830452c33b535366e7b78c5ba5de3d6e4256
Aljon Cadiz <AljonC@Magenic.com>
2017-12-21 17:30:46 +0800

    Tut1: Angular Module Basics

--- angular4-app/src/app/app.module.ts
+++ angular4-app/src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TeamListComponent } from './teams/team-list/team-list.component';
+import { PlayerListComponent } from './players/player-list/player-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamListComponent,
+    PlayerListComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'teams',
+        component: TeamListComponent
+      },
+      {
+        path: 'players',
+        component: PlayerListComponent
+      },
+      {
+        path: '',
+        component: PlayerListComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
